320ca5f9a1aab0a113ed9f727af97cab35dc2796
User <davidhartmann2010@gmail.com>
2025-05-03 20:03:50 +0200

    new folder orga

---
+++ configure/apply_config.bat
@@ -0,0 +1,7 @@
+copy .\config_files\modified\DHA_appsettings.json ..\SOURCE\DHA\appsettings.json
+copy .\config_files\modified\DHA_settingsLog4Net.xml ..\SOURCE\DHA\settingsLog4Net.xml
+copy .\config_files\modified\DHAEFModels_appsettings.json ..\SOURCE\DHA.EntityFrameworkCore_Models\appsettings.json
+copy .\config_files\modified\DHAEFModelsInitializer_appsettings.json ..\SOURCE\DHA.EntityFrameworkCore_Models.Initializer\appsettings.json
+copy .\config_files\modified\DHAFun_settingsLog4Net.xml ..\SOURCE\DHA.Fun\settingsLog4Net.xml
+
+pause
