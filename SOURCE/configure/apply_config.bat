3e5b31b3d0d7184fc3fc319d0f2f1aa53aff45c5
hartmann <davidhartmann2010@gmail.com>
2025-05-16 18:48:52 +0200

    update

---
+++ SOURCE/configure/apply_config.bat
@@ -0,0 +1,6 @@
+copy .\config_files\modified\DHAFRONTMVC_appsettings.json ..\DHA.FRONT.MVC\appsettings.json
+copy .\config_files\modified\DHAFRONTMVC_settingsLog4Net.xml ..\DHA.FRONT.MVC\settingsLog4Net.xml
+copy .\config_files\modified\DHADALINIT_appsettings.json ..\DHA.DAL.INIT\appsettings.json
+copy .\config_files\modified\DHAFUN_settingsLog4Net.xml ..\DHA.Fun\settingsLog4Net.xml
+
+pause
