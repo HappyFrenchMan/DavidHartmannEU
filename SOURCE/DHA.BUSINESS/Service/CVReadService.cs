f95458142e1d3c2196ad1960962bbff5c827213c
hartmann <davidhartmann2010@gmail.com>
2025-06-02 18:38:44 +0200

    en cours

--- SOURCE/DHA.BUSINESS/Service/CVReadService.cs
+++ SOURCE/DHA.BUSINESS/Service/CVReadService.cs
@@ -24,68 +24,64 @@ namespace DHA.BUSINESS.Service
 
         public List<SkillStatBM> readSkillStatM()
         {
+
+
+
+
+            List<Tuple<string, TimeSpan, string>> __tupleResult = new List<Tuple<string, TimeSpan, string>>();
+
+
+            List<CV_Skill> __lstSkill = MyDatabase.CVSkillRepository.GetAll().Include("Type").ToList();
+            List<CV_ActivitySkill> __lstActivitySkill = MyDatabase.CVActivitySkillRepository.GetAll().ToList();
+            List<CV_Activity> __lstActivity = MyDatabase.CVActivityRepository.GetAll().ToList();
+            List<CV_Experience> __lstExperience = MyDatabase.CVExperienceRepository.GetAll().ToList();
+            List<int> __listIdExperienceFound = new List<int>();
+
+            foreach (CV_Skill __skill in __lstSkill)
+            {
+                __listIdExperienceFound.Clear();
+                TimeSpan __tsDuration = new TimeSpan();
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (CV_ActivitySkill __activitySkill in __lstActivitySkill)
+                {
+                    if (__activitySkill.SkillId.Equals(__skill.ID))
+                    {
+                        foreach (CV_Activity __activity in __lstActivity)
+                        {
+                            if (__activity.Id.Equals(__activitySkill.ActivityId))
+                            {
+                                foreach (CV_Experience __experience in __lstExperience)
+                                {
+                                    if (__experience.ID.Equals(__activity.ExperienceId)
+                                        && !__listIdExperienceFound.Contains(__activity.ExperienceId))
+                                    {
+                                        __tsDuration += __experience.ExperiencePeriod.DureeExperience();
+
+                                        if (stringBuilder.Length > 0)
+                                        {
+                                            stringBuilder.Append("##");
+                                        }//if
+                                        stringBuilder.Append(__experience.Name + "-" + __experience.Description);
+                                        __listIdExperienceFound.Add(__experience.ID);
+                                    }//if
+                                }//foreach
+                            }//if
+                        }//foreach                          
+                    }//if
+                }//foreach
+                Tuple<string, TimeSpan, string> __TupleNew
+                    = Tuple.Create($"{__skill.Name}[##][{__skill.Type.Description}]",
+                    __tsDuration,
+                    stringBuilder.ToString());
+
+                __tupleResult.Add(__TupleNew);
+            }//foreach
+
             List<SkillStatBM> __lstSkillStatM = new List<SkillStatBM>();
-            foreach (Tuple<string, TimeSpan, string> skillstat in MyStatistic.select_skill_by_duration())
+            foreach (Tuple<string, TimeSpan, string> skillstat in __tupleResult)
             {
                 __lstSkillStatM.Add(SkillStatBM.ToSkillStatBM(skillstat));
             }//foreach
-
-
-            public static List<Tuple<string, TimeSpan, string>> select_skill_by_duration()
-        {
-            List<Tuple<string, TimeSpan, string>> __tupleResult = new List<Tuple<string, TimeSpan, string>>();
-
-            using (DHA_Db_Context lDHA_Db_Context = new DHA_Db_Context())
-            {
-                List<Skill> __lstSkill = lDHA_Db_Context.Skills.Include(a => a.Type).ToList();
-                List<ActivitySkill> __lstActivitySkill = lDHA_Db_Context.ActivitySkills.ToList();
-                List<Activity> __lstActivity = lDHA_Db_Context.Activities.ToList();
-                List<Experience> __lstExperience = lDHA_Db_Context.Experiences.ToList();
-                List<int> __listIdExperienceFound = new List<int>();
-
-                foreach (Skill __skill in __lstSkill)
-                {
-                    __listIdExperienceFound.Clear();
-                    TimeSpan __tsDuration = new TimeSpan();
-                    StringBuilder stringBuilder = new StringBuilder();
-                    foreach (ActivitySkill __activitySkill in __lstActivitySkill)
-                    {
-                        if (__activitySkill.SkillId.Equals(__skill.ID))
-                        {
-                            foreach (Activity __activity in __lstActivity)
-                            {
-                                if (__activity.ID.Equals(__activitySkill.ActivityId))
-                                {
-                                    foreach (Experience __experience in __lstExperience)
-                                    {
-                                        if (__experience.ID.Equals(__activity.ExperienceId)
-                                            && !__listIdExperienceFound.Contains(__activity.ExperienceId))
-                                        {
-                                            __tsDuration += __experience.ExperiencePeriod.DureeExperience();
-
-                                            if (stringBuilder.Length > 0)
-                                            {
-                                                stringBuilder.Append("##");
-                                            }//if
-                                            stringBuilder.Append(__experience.Name + "-" + __experience.Description);
-                                            __listIdExperienceFound.Add(__experience.ID);
-                                        }//if
-                                    }//foreach
-                                }//if
-                            }//foreach                          
-                        }//if
-                    }//foreach
-                    Tuple<string, TimeSpan, string> __TupleNew
-                        = Tuple.Create($"{__skill.Name}[##][{__skill.Type.Description}]",
-                        __tsDuration,
-                        stringBuilder.ToString());
-
-                    __tupleResult.Add(__TupleNew);
-                }//foreach
-            }//using
-
-            return __tupleResult.OrderByDescending(a => a.Item2).ToList();
-        }//select_experiences
 
             return __lstSkillStatM;
         }//readSkillStat
